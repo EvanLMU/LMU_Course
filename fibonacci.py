35a21539e272a74b2f8e36b78e397fd6c4c692f1
Evan <evaneames7@gmail.com>
2026-04-18 23:18:03 +0200

    print statement in fibonnaci function and count function

--- fibonacci.py
+++ fibonacci.py
@@ -3,6 +3,12 @@ def fibonnaci(n):
     while n != 1:
         n -= 1
         fib = fib*n
-    return fib
+    print("The fibonnaci value of " + str(n) + " is " + str(fib))
+    return None
 
-print(fibonnaci(5))
+def count(n):
+    for i in range(n):
+        print(n)
+
+count(5)
+fibonnaci(5)
